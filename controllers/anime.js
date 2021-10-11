36c94e1cc63c1f7de24e2c426384fd72410beb8c
Rujaxx <shaikhrujan@gmail.com>
2021-10-12 01:52:27 +0530

    added genre search

--- controllers/anime.js
+++ controllers/anime.js
@@ -2,11 +2,68 @@ const ANIAPI = require("@mattplays/aniapi");
 const asyncHandler = require('../middleware/async')
 const ErrorResponse = require('../utils/errorResponse')
 const API = new ANIAPI.API("DUMMY_JWT");
+const Review = require('../models/Review')
 
-exports.getAnime = asyncHandler(async (req,res,next) => {
+// @desc      Add a review
+// @route     POST /api/v1/anime/:id
+// @access    Private
+exports.addReview = asyncHandler(async (req,res,next) => {
+    let id = req.params.id
+
+    const anime = await API.Anime.GetByID(id)
+
+    if(!anime){
+        return next(new ErrorResponse(`Anime with given ${id} not found`,400))
+    }
+
+    req.body.user = req.user.id;
+    req.body.anime = id
+
+    const review = await Review.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        data: review,
+        anime : anime
+    });
+})
+
+// @desc      GET one anime
+// @route     GET /api/v1/anime/:id
+// @access    Private
+exports.getOne = asyncHandler(async (req,res,next) => {
+    let id = req.params.id
+    const anime = await API.Anime.GetByID(id)
+
+    if(!anime){
+        return next(new ErrorResponse(`Anime with given ${id} not found`,400))
+    }
+
+    const reviews = await Review.find({anime : id});
+
+    res.status(200).json({ success : true , data : anime , reviews : reviews})
+})
+
+// @desc      GET anime
+// @route     GET /api/v1/anime
+// @access    Private
+exports.getAnimeByTitle = asyncHandler(async (req,res,next) => {
+    console.log(req.query.title)
     const animes = await API.Anime.Get({
+        title : req.query.title,
         per_page : 20
     })
-    console.log(req.cookies.token)
+    res.status(200).json({ success : true , data : animes})
+})
+
+// @desc      GET anime
+// @route     GET /api/v1/anime
+// @access    Private
+exports.getAnime = asyncHandler(async (req,res,next) => {
+    console.log(req.query.title)
+    const animes = await API.Anime.Get({
+        genres : req.query.genres,
+        per_page : 20
+    })
     res.status(200).json({ success : true , data : animes})
 })
--- routes/anime.js
+++ routes/anime.js
@@ -1,11 +1,17 @@
 const express = require('express')
 
-const { getAnime } = require('../controllers/anime')
+const { getAnime,addReview,getOne,getAnimeByTitle } = require('../controllers/anime')
 
 const router = express.Router()
 
 const { protect } = require('../middleware/auth')
 
-router.route('/').get(protect,getAnime)
+router.route('/')
+.get(protect,getAnime)
+
+router.route('/title')
+.get(protect,getAnimeByTitle)
+
+router.route('/:id').post(protect,addReview).get(protect,getOne)
 
 module.exports = router
